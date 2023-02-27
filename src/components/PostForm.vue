47c1996867f9368af47844e6f7bfde2e8493557b
Mohammed Aamir <m.aamir@domain.rage-india.com>
2023-02-27 10:00:49 +0530

    Initial commit

---
+++ src/components/PostForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container mt-5">
+    <form @submit.prevent="submitForm">
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <input
+            v-model="post.title"
+            class="input"
+            type="text"
+            placeholder="Title"
+          />
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Description</label>
+        <div class="control">
+          <textarea
+            v-model="post.content"
+            class="textarea"
+            placeholder="..."
+          ></textarea>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Creator name</label>
+        <div class="control">
+          <input
+            v-model="post.creator"
+            class="input"
+            type="text"
+            placeholder="Creator name"
+          />
+        </div>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button type="submit" class="button is-primary">Submit</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['post', 'submitForm'],
+}
+</script>
+
+<style></style>
